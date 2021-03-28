313770306f63818a3fc5e8b8fb2769302b581332
violetbaggins <violetbaggins@gmail.com>
2021-03-28 11:54:13 -0300

    todo a testing

---
+++ public/js/dvd.js
@@ -0,0 +1,77 @@
+window.addEventListener('load', function () {
+
+    let body = document.querySelector('body')
+    let container = document.getElementsByClassName('container')
+    
+    let botonDeMierda = document.getElementById('malisimo');
+    
+
+  
+
+    
+
+    
+
+    botonDeMierda.addEventListener('click', () =>{
+    
+        // mostrar div bluescreen
+        body.innerHTML += `<div id="bluescreen" class="bluescreen"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/DVD_logo.svg/1280px-DVD_logo.svg.png" width="200px" alt="" id="dvdimg"></div>`
+        
+        // <a href="/">Volve a empezar por discriminador de un front pedorro</a><button id="stop" class="malisimo">Para para para!!!</button>
+        
+    // agrega body-bluescreen al body
+        body.classList.add("body-bluescreen")
+    
+    // movimiento dvdimg
+        
+        let bluescreen = document.getElementById("bluescreen")
+        let btnStop = document.getElementById("stop")
+        let dvdimg = document.getElementById("dvdimg")
+            dvdimg.style.left = 0
+            dvdimg.style.top = 0
+        let moveX = 10
+        let moveY = 10
+
+        console.log("Andando");  
+
+        let intervalMove = setInterval(moveDVD, 100)
+
+        function moveDVD() {
+    
+            dvdimg.style.left = parseInt(dvdimg.style.left) + moveX + 'px'
+            dvdimg.style.top = parseInt(dvdimg.style.top) + moveY + 'px'
+
+        //    si alcanza del ancho, cambiar la velocidad a -10????
+
+        if (parseInt(dvdimg.style.top) >= window.innerHeight - 108){
+            moveY = -10
+            
+        }
+
+        if (parseInt(dvdimg.style.left) >= window.innerWidth - 205){
+            
+            moveX = -10
+        }
+
+        if (parseInt(dvdimg.style.top) <= 0){
+            moveY = 10
+        }
+
+        if (parseInt(dvdimg.style.left) <= 0){
+            moveX = 10
+        }
+
+            // console.log("left: ", dvdimg.style.left);
+            // console.log("left: ", dvdimg.style.top);
+        }
+
+        /* btnStop.addEventListener('click', () => {
+            clearInterval(intervalMove);
+            bluescreen.innerHTML += `<a href="/">Volve a empezar por discriminador de un front pedorro</a>`
+        }) */
+        
+
+    })
+
+})
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,23 @@
 window.addEventListener('load', function () {
 
+    let btnRomper = document.getElementById("btn-romper")
+    let container = document.querySelector(".container")
+    let body = document.querySelector("body")
     
-    // let jerry = document.getElementById('ag-jerry')
-    
-    // jerry.addEventListener('click', ()=>{
-    //     jerry.style.backgroundColor = "yellow"
-    // })
-    
+
+    btnRomper.addEventListener('click', () => {
+      container.classList.add("romper")
+      /* container.innerHTML += "<h1>Pillin, pillin. Ves lo que pasa cuando rompes!!! espera unos segundos mas y vas a ver: homero simpson</h1>" */
+
+      if (container.classList.contains("romper")){
+
+        setTimeout(() => {
+
+          body.classList.add("backwards")
+
+        }, 2000)
+      }
+
+    })
 
 })
